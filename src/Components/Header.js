c4e4a32e91ad79b58f94906a679e72efeefe4911
mindxploit <bobbonicola@gmail.com>
2020-10-24 23:56:57 +0200

    removed header on mobile

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
+import styled from 'styled-components';
 
 const Header = () => {
+	
+	const TextContainer = styled.div`
+		margin: 0 2em;
+		@media (max-width: 480px) {
+			display: none;
+		}
+	`
+	
 	return (
-		<div style={{ margin: '0 2em' }}>
+		<TextContainer>
 			<Typography align="center" variant="h1">
 				Welcome, let’s pick a key and start playing!
 			</Typography>
-		</div>
+		</TextContainer>
 	);
 };
 
